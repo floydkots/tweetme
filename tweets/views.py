e299e81b4f6e8ad9fe91a120d2158642fb424e3f
Floyd Kots <floydkotohoyoh@gmail.com>
2017-09-18 14:10:26 +0300

    Update: Added simple queries to provide context to the tweet views

--- tweets/views.py
+++ tweets/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from .models import Tweet
 
 # Create your views here.
 
 
 # Retrieve tweet
 def tweet_detail_view(request, id=1):
-    return render(request, "tweets/detail_view.html", {})
+    obj = Tweet.objects.get(id=id) # Get from database
+    context = {
+        "object": obj
+    }
+    return render(request, "tweets/detail_view.html", context)
 
 
 # List tweets
 def tweet_list_view(request):
-    return render(request, "tweets/list_view.html", {})
+    queryset = Tweet.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "tweets/list_view.html", context)
