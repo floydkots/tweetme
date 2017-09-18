d26efdbd321db2ef2f16ea59e5513503cc58ec8e
Floyd Kots <floydkotohoyoh@gmail.com>
2017-09-18 15:14:20 +0300

    Experimented with class based views

--- tweets/urls.py
+++ tweets/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 
 from .views import tweet_detail_view, tweet_list_view
 
+from tweets.views import TweetDetailView, TweetListView
+
 urlpatterns = [
-    url(r'^$', tweet_list_view, name='list'),
-    url(r'^1/$', tweet_detail_view, name='detail')
+    url(r'^$', TweetListView.as_view(), name='list'),
+    url(r'^1/$', TweetDetailView.as_view(), name='detail')
 ]
 
 
--- tweets/views.py
+++ tweets/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 from .models import Tweet
 
 # Create your views here.
 
 
 # Retrieve tweet
+
+class TweetDetailView(DetailView):
+    queryset = Tweet.objects.all()
+    template_name = "tweets/detail_view.html"
+
+    def get_object(self):
+        return Tweet.objects.get(id=1)
+
+
+class TweetListView(ListView):
+    template_name = "tweets/list_view.html"
+    queryset = Tweet.objects.all()
+
 def tweet_detail_view(request, id=1):
     obj = Tweet.objects.get(id=id)  # Get from database
     context = {
